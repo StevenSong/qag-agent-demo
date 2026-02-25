ec6c2339ad6b89872dfce3179d6b9f847aac79a3
Steven Song <steven.song98@gmail.com>
2026-02-25 17:57:23 -0600

    enable bulk inference using agent.py

--- src/agent.py
+++ src/agent.py
@@ -1,17 +1,21 @@
 import argparse
+import asyncio
 
+import pandas as pd
 import uvicorn
 from pydantic import BaseModel
 from pydantic_ai import Agent
 from pydantic_ai.models.openai import OpenAIChatModel
 from pydantic_ai.providers.openai import OpenAIProvider
 from pydantic_ai.toolsets.fastmcp import FastMCPToolset
+from tqdm.asyncio import tqdm
 
 
 def make_agent(
     mcp_url: str,
     llm_url: str,
     output_type: BaseModel | None = None,
+    retries: int = 1,
 ) -> Agent:
     toolset = FastMCPToolset(mcp_url)
     model = OpenAIChatModel(
@@ -30,6 +34,8 @@ def make_agent(
             "Also math is hard for LLMs to do in natural language. If you need to do arithmetic, just use the tools."
         ),
         output_type=output_type,
+        max_concurrency=4,
+        retries=retries,
     )
 
     @agent.tool_plain
@@ -63,6 +69,10 @@ def make_agent(
     return agent
 
 
+class BulkEvalResult(BaseModel):
+    percentage: float
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-q", "--query")
@@ -80,10 +90,9 @@ if __name__ == "__main__":
             f"Cannot use both --query ({args.query}) and --port ({args.port}), setting port implies serving the agent UI while setting query implies running the agent over provided inputs"
         )
 
-    agent = make_agent(mcp_url=args.mcp_url, llm_url=args.llm_url)
-
     if args.query is None:
         # server the agent with a UI
+        agent = make_agent(mcp_url=args.mcp_url, llm_url=args.llm_url)
         app = agent.to_web()
         port = 8000  # default
         if args.port is not None:
@@ -91,9 +100,32 @@ if __name__ == "__main__":
         uvicorn.run(app, port=port)
     elif args.query.endswith(".csv"):
         # bulk inference
-        pass
+        agent = make_agent(
+            mcp_url=args.mcp_url,
+            llm_url=args.llm_url,
+            output_type=BulkEvalResult,
+            retries=3,
+        )
+        df = pd.read_csv(args.query)
+        queries = [
+            q
+            + " You should return a single percentage as the final answer without any extraneous text."
+            for q in df["questions"]
+        ]
+
+        async def run_all_queries():
+            queued = [agent.run(q) for q in queries]
+            results = await tqdm.gather(*queued)
+            out = pd.Series(
+                [r.output.percentage for r in results],
+                name="answers",
+            )
+            out.to_csv(args.query.replace(".csv", "-answers.csv"), index=False)
+
+        asyncio.run(run_all_queries())
     else:
         # run single query
+        agent = make_agent(mcp_url=args.mcp_url, llm_url=args.llm_url)
         result = agent.run_sync(args.query)
         for message in result.all_messages():
             print("====================================================")
